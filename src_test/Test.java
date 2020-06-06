93c0618cc958563116138a2538d5a4299b4d9eb8
lizhen <2290109726@qq.com>
2020-06-07 00:01:49 +0800

    1、添加了JSON依赖包 2、基本登录功能完成 3、创建User、Course实体类

--- src_test/Test.java
+++ src_test/Test.java
@@ -1,12 +1,10 @@
-import bean.Student;
+
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student s=new Student();
-		s.setUserName("test001");
-		System.out.print(s.login("222333"));
+		
 
 	}
 
